ba17d37e12d62231e4e6c2c3e5fd4414b4ac9cce
ranon rat <66473662+ranon-rat@users.noreply.github.com>
2023-04-24 17:20:44 -0600

    Update player.cpp

--- src/player.cpp
+++ src/player.cpp
@@ -7,18 +7,17 @@
 
 #include "global.hpp"
 
-
 void Player::Move(SDL_Event event) {
   if (event.type != SDL_KEYDOWN) return;
   switch (event.key.keysym.sym) {
     case SDLK_w:
-      this->y += sin(horizontal_angle*RAD) ;
-      this->x += cos(horizontal_angle*RAD) ;
+      this->y += sin(horizontal_angle * RAD) * 0.1;
+      this->x += cos(horizontal_angle * RAD) * 0.1;
       break;
     case SDLK_s:
 
-      this->y -= sin(horizontal_angle*RAD) ;
-      this->x -= cos(horizontal_angle*RAD) ;
+      this->y -= sin(horizontal_angle * RAD) * 0.1;
+      this->x -= cos(horizontal_angle * RAD) * 0.1;
       break;
     case SDLK_a:
 
@@ -31,21 +30,21 @@ void Player::Move(SDL_Event event) {
 
       break;
     case SDLK_SPACE:
-    this->z+=1;
-    break;
+      this->z += 0.1;
+      break;
     case SDLK_LSHIFT:
-    this->z-=1;
-    break;
+      this->z -= 0.1;
+      break;
     default:
       break;
   }
 
   if (horizontal_angle < 0) {
-    horizontal_angle += 360 ;
+    horizontal_angle += 360;
   }
 
   if (horizontal_angle >= 360) {
-    horizontal_angle -=360;
+    horizontal_angle -= 360;
   }
 }
 
@@ -53,14 +52,11 @@ void Player::Show(SDL_Renderer *renderer) {
   // SDL2 Not supports circles
   SDL_Rect rect = {(int)this->x * 10 - 5, (int)this->y * 10 - 5, 10, 10};
   SDL_Color color = {100, 0, 0, 100};
-    SDL_SetRenderDrawColor(renderer, color.r, color.g, color.b, 255);
-  SDL_RenderDrawRect(renderer,&rect);
-
+  SDL_SetRenderDrawColor(renderer, color.r, color.g, color.b, 255);
+  SDL_RenderDrawRect(renderer, &rect);
 }
 
 void Player::SetPos(float i_x, float i_y) {
   x = i_x + .5;
   y = i_y + 0.5;
 }
-
-  
